45f43e1233158a2161974c3a54ccfb4663ad4209
Kasper <kasper.veliz@gmail.com>
2025-01-14 12:46:17 -0500

    setup

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def main():
-    print("Hello World")
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+import yahoo_fin.stock_info as si
 
-main()
+style.use('ggplot')
+start = dt.datetime(2020, 1, 1)
+end = dt.datetime(2024, 12, 31)
+
+snp_list = si.tickers_sp500()
+print("tickers sp500: " + str(len(snp_list)))
+print(snp_list[0:10])
