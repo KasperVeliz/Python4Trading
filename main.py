f5600a42e00c9ad1a4c4f30f09c5ec8f79ad5ed4
Kasper <kasper.veliz@gmail.com>
2025-01-17 07:32:37 -0500

    setup

--- main.py
+++ main.py
@@ -4,11 +4,26 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 import yahoo_fin.stock_info as si
+import backtrader as bt
+from pandas_datareader.wb import download
 
 style.use('ggplot')
 start = dt.datetime(2020, 1, 1)
 end = dt.datetime(2024, 12, 31)
 
-snp_list = si.tickers_sp500()
-print("tickers sp500: " + str(len(snp_list)))
-print(snp_list[0:10])
+dow_list = si.tickers_dow()
+historical_datas = {}
+for ticker in dow_list:
+    historical_datas[ticker] = si.get_data(ticker, start_date=start, end_date=end, interval='1d')
+
+if __name__ == '__main__':
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(10000)
+    data = bt.feeds.PandasData
+    cerebro.adddata(dow_list)
+
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.run()
+
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
